473fdff8d8539c0c9566fd820144c7468ff3a9c3
Finbarr <Finbarr.Oconnell12345@gmail.com>
2019-12-12 21:18:16 -0800

    initial commit

---
+++ Expedition/ViewController.swift
@@ -0,0 +1,152 @@
+//
+//  ViewController.swift
+//  Expedition
+//
+//  Created by Zeqiel Golomb on 8/16/19.
+//  Copyright © 2019 The Morning Company All rights reserved.
+//
+//
+
+import UIKit
+import WebKit
+import Foundation
+
+class ViewController: UIViewController, WKNavigationDelegate, UISearchBarDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var ActInd: UIActivityIndicatorView!
+    
+    var History: Array<Any>!
+    var historyRuns = 0
+    let credits: String = "Zeqe Golomb, Finbarr O'Connell, Jackson Yan, Julian Wright, Brendan Burkhart, Kai Morita-McVey"
+    
+    var components = URLComponents(string: "https://duckduckgo.com/")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //let searchEngine: String = "duckduckgo.com"
+        
+        
+        let url = URL(string: "https://duckduckgo.com/")
+        
+        let request = URLRequest(url: url!)
+        
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "duckduckgo.com"
+        
+        webView.load(request)
+        
+        webView.addSubview(ActInd)
+        ActInd.startAnimating()
+        
+        webView.navigationDelegate = self
+        ActInd.hidesWhenStopped = true
+    }
+    
+    
+    @IBAction func back(_ sender: Any) {
+        if webView.canGoBack{
+            webView.goBack()
+        }
+    }
+
+    @IBAction func forward(_ sender: Any) {
+        if webView.canGoForward{
+            webView.goForward()
+        }
+    }
+
+    
+    @IBAction func refresh(_ sender: Any) {
+        webView.reload()
+    }
+    
+    @IBAction func pinchReload(_ sender: Any) {
+        webView.reload()
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        webView.stopLoading()
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        
+        ActInd.startAnimating()
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        ActInd.stopAnimating()
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        
+        ActInd.stopAnimating()
+        
+    }
+    
+    func verifyUrl (urlString: String?) -> Bool { //tests for url
+        if (urlString!.contains(".")) {
+                if let url = URL(string: "http://" + urlString!) {
+                    return UIApplication.shared.canOpenURL(url)
+                }else {
+                    return false
+                }
+            }
+            return false
+        }
+    
+    func historyOnOff() -> Bool {
+        let historyOnOff = true
+        return historyOnOff
+    }
+
+    func history(urlForHistory : String?) {
+        if historyOnOff(){
+            if (historyRuns == 0) {
+                History = [urlForHistory!]
+            } else {
+                History.append(urlForHistory!)
+            }
+            historyRuns += 1
+        }
+    }
+        
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        var urlForHistory = searchBar.text
+        history(urlForHistory : searchBar.text)
+        
+        searchBar.resignFirstResponder()
+        
+        if (verifyUrl(urlString: searchBar.text!)) {
+            let url = URL(string: "http://\(searchBar.text!)")
+            
+            
+             let request = URLRequest(url: url!)
+            
+            webView.load(request)
+        }
+        else {
+            let queryItemQuery = URLQueryItem(name: "q", value: searchBar.text);
+            
+            components?.queryItems = [queryItemQuery]
+            
+            let request = URLRequest(url: (components?.url)!)
+            
+            webView.load(request)
+        }
+          
+    }
+    
+    
+}
+    
+func DarkModeToggle(_ sender: Any) {
+    }
