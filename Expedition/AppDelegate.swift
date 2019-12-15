90bfc5d36d8a7edebf9f9fbb9f37ad2d3b55a4af
Zeqe G <zeqe.g@icloud.com>
2019-12-14 17:35:59 -0800

    UI/UX changes

--- Expedition/AppDelegate.swift
+++ Expedition/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -38,11 +39,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
+       // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+      
+    
+        }
  
 
 }
 
 }
+
 
--- Expedition/ViewController.swift
+++ Expedition/ViewController.swift
@@ -11,21 +11,20 @@ import UIKit
 import WebKit
 import Foundation
 
-class ViewController: UIViewController, WKNavigationDelegate, UISearchBarDelegate {
+class ViewController: UIViewController, WKNavigationDelegate, UISearchBarDelegate, UISceneDelegate, UIWindowSceneDelegate {
     
     @IBOutlet weak var webView: WKWebView!
     
     @IBOutlet weak var ActInd: UIActivityIndicatorView!
     
-    var History: Array<Any>!
+    var History: Array<Any>! //history Array initialization
     var historyRuns = 0
     let credits: String = "Zeqe Golomb, Finbarr O'Connell, Jackson Yan, Julian Wright, Brendan Burkhart, Kai Morita-McVey"
     
-    var components = URLComponents(string: "https://duckduckgo.com/")
+    var components = URLComponents(string: "https://duckduckgo.com/") //search engine
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad() //setup stuff
         
         //let searchEngine: String = "duckduckgo.com"
         
@@ -48,28 +47,32 @@ class ViewController: UIViewController, WKNavigationDelegate, UISearchBarDelegat
     }
     
     
-    @IBAction func back(_ sender: Any) {
+    @IBAction func back(_ sender: Any) { //makes the page go back if it can
         if webView.canGoBack{
             webView.goBack()
         }
     }
 
-    @IBAction func forward(_ sender: Any) {
+    @IBAction func forward(_ sender: Any) { //makes the page go forward if it can
         if webView.canGoForward{
             webView.goForward()
         }
     }
 
     
-    @IBAction func refresh(_ sender: Any) {
+    @IBAction func refresh(_ sender: Any) { //makes the page reload
         webView.reload()
     }
     
-    @IBAction func pinchReload(_ sender: Any) {
-        webView.reload()
+    @IBAction func swipeReload(_ sender: Any) {
+   
+        webView.reloadFromOrigin()
+    
     }
     
-    @IBAction func stop(_ sender: Any) {
+
+    
+    @IBAction func stop(_ sender: Any) { //makes the page stop loading
         webView.stopLoading()
     }
     
@@ -92,35 +95,50 @@ class ViewController: UIViewController, WKNavigationDelegate, UISearchBarDelegat
     }
     
     func verifyUrl (urlString: String?) -> Bool { //tests for url
-        if (urlString!.contains(".")) {
-                if let url = URL(string: "http://" + urlString!) {
-                    return UIApplication.shared.canOpenURL(url)
-                }else {
-                    return false
+        let url: URL?
+        if urlString!.hasPrefix("http://") {
+            url = URL(string: urlString!)
+        } else {
+            url = URL(string: "http://" + urlString!)
+        }
+        if let url = url {
+            if (urlString!.contains(".")) {
+                if (UIApplication.shared.canOpenURL(url)) {
+                    return true
                 }
             }
-            return false
         }
-    
-    func historyOnOff() -> Bool {
+        return false
+    }
+    func historyOnOff() -> Bool { //will be connected to a switch turning tracking on and off
         let historyOnOff = true
         return historyOnOff
     }
 
-    func history(urlForHistory : String?) {
+    func history(urlForHistory : String?) { //adds stuff to the history array
         if historyOnOff(){
             if (historyRuns == 0) {
-                History = [urlForHistory!]
+                History = [urlForHistory!] // it is used ignore ->
             } else {
                 History.append(urlForHistory!)
             }
             historyRuns += 1
         }
     }
+    
+    func searchText(urlString: String) -> URLRequest { //julian please explain
+        let queryItemQuery = URLQueryItem(name: "q", value: urlString);
         
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        components?.queryItems = [queryItemQuery]
         
-        var urlForHistory = searchBar.text
+        let request = URLRequest(url: (components?.url)!)
+        
+        return request
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) { //turns the users input into something that the search engine can use
+        
+        var urlForHistory = searchBar.text // it is used ignore ->
         history(urlForHistory : searchBar.text)
         
         searchBar.resignFirstResponder()
@@ -129,12 +147,12 @@ class ViewController: UIViewController, WKNavigationDelegate, UISearchBarDelegat
             let url = URL(string: "http://\(searchBar.text!)")
             
             
-             let request = URLRequest(url: url!)
+            let request = URLRequest(url: url!)
             
             webView.load(request)
         }
         else {
-            let queryItemQuery = URLQueryItem(name: "q", value: searchBar.text);
+            let queryItemQuery = URLQueryItem(name: "q", value: searchBar.text!);
             
             components?.queryItems = [queryItemQuery]
             
@@ -148,5 +166,7 @@ class ViewController: UIViewController, WKNavigationDelegate, UISearchBarDelegat
     
 }
     
-func DarkModeToggle(_ sender: Any) {
-    }
+func DarkModeToggle(_ sender: Any) { //turns dark mode on and off
+    
+}
+
